175c3f07212e1027a2fe51294de4ee36369f8d38
aliibrahim095 <4liebrahim@gmail.com>
2021-08-07 12:44:31 +0200

    Navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+export default function Navbar() {
+  const classes = useStyles();
+  const history=useHistory()
+  const goHome=(e)=>{
+      e.preventDefault();
+      history.push('/');
+  }
+  const goActive=(e)=>{
+      e.preventDefault();
+      history.push('/');
+  }
+  const goCompleted=(e)=>{
+      e.preventDefault();
+      history.push('/');
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            TODO
+          </Typography>
+          <Button onClick={goHome} color="inherit">Home</Button>
+          <Button onClick={goActive} color="inherit">Active</Button>
+          <Button onClick={goCompleted} color="inherit">Completed</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ src/views/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Hello Home
+        </div>
+    )
+}
+
+export default Home
